7d1e1952a0bb7a69955c20a64c297f60d9bc260b
Bowgo Tsai <bowgotsai@google.com>
2019-12-16 11:36:45 +0800

    axolotl: Adding developer GSI keys
    Bug: 144399552
    Test: tree-hugger
    
    Change-Id: I0cf78bfc80c24bb217bb88831faa5a29ba04d194

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Inherit vendor blobs
 $(call inherit-product, vendor/shift/axolotl/axolotl-vendor.mk)
 
