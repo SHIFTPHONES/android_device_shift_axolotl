a1b7edd7a192b595f4528807e081a92a6fcc1958
Alexander Martinz <amartinz@shiftphones.com>
2024-01-16 10:17:08 +0100

    axolotl: include clearkey service definition
    Change-Id: I8c5214af745053be131863cbf449f4e74285eec8
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -184,9 +184,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0-service \
 
 # DRM
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey \
+include frameworks/av/drm/mediadrm/plugins/clearkey/service.mk
 
 # Fastbootd
 PRODUCT_PACKAGES += \
