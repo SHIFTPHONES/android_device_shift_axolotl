6fa8a73e75b70d49d5b130570d18a96381571501
Alexander Martinz <amartinz@shiftphones.com>
2024-12-05 13:36:16 +0100

    axolotl: import EuiccOverlay
    Change-Id: I416c82090feaf4f8ff78147d79ca314ac3bf63a1
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

---
+++ rro_overlays/EuiccOverlay/src/eco/shift/euiccoverlay/EuiccReceiver.java
@@ -0,0 +1,19 @@
+package eco.shift.euiccoverlay;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This is a stub to register partner customizations.
+ */
+public class EuiccReceiver extends BroadcastReceiver {
+     public EuiccReceiver() {
+          // Empty
+     }
+
+     @Override
+     public void onReceive(Context context, Intent intent) {
+          // Empty
+     }
+}
